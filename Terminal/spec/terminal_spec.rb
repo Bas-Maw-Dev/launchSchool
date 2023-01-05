cc19e96e66e286242f3930d23a1a7020299fb978
Bas Maw <115073747+Bas-Maw-Dev@users.noreply.github.com>
2023-01-05 14:28:31 +0000

    Red - NameError

--- Terminal/spec/terminal_spec.rb
+++ Terminal/spec/terminal_spec.rb
@@ -2,6 +2,8 @@ require_relative '../terminal'
 
 describe 'termnal exists' do
   it 'outputs a -terminal- screen' do
-    
+    terminal = Terminal.new
+    message = 'Hello World!'
+    expect(terminal.output(message)).to include(message)
   end
 end
